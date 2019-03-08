add30a648d7f853b481c4fd499c49ab00dd9955c
danireyes5015@gmail.com <50138013>
2019-03-08 12:49:54 +0100

    archivos

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('templates.plantilla')
+@section('titulo','Menú Principal')
+@section('contenido')
+ <div class="container-fuid display-table">
+     <div class="row display-table-row">
+         <div class="col-10 display-table-cell valign-top" id="menulateral"> 
+            <!-- parte del menu lateral -->
+            <h1>Navigation</h1>
+         </div>
+         <div class="col-10 box display-table-cell valign-top">
+        <!-- parte del menu principal -->
+        <p>Chanante ipsum dolor sit amet, ex ea vivo con tu madre en un castillo tempor nostrud coconut bufonesco eiusmod ex. Zanguango, nisi churretoso regomello eveniet exercitation eiusmod sed nostrud síberet nisi tempor sed labore. Consectetur adipisicing saepe aliqua elit ea adipisicing adipisicing. One more time saepe, no te digo ná y te lo digo tó eiusmod nostrud.</p>
+         </div>
+     </div>
+ </div>   
+@endsection
